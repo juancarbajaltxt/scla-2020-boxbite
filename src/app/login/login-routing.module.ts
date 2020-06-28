6db2945676d6ad71bceb63064a4452d67751aada
TXT-00 <txt-00@macbook-pro-6.home>
2020-06-27 18:50:40 -0700

    I create the menu page and button for place order

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,20 +2,15 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./restaurant/restaurant.module').then( m => m.RestaurantPageModule)
-  },
-  {
-    path: 'checkout',
-    loadChildren: () => import('./checkout/checkout.module').then( m => m.CheckoutPageModule)
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+
+export class AppRoutingModule { }
---
+++ src/app/restaurant/restaurant.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant',
+  templateUrl: './restaurant.page.html',
+  styleUrls: ['./restaurant.page.scss'],
+})
+export class RestaurantPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+var restaurant = [{
+  name: 'adelmo', 
+ address: '1114 los angeles',
+ category: 'salvadoran'
+}]; 
+
+ function checkout (){ 
+
+ };
