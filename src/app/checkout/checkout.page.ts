0bd20758f51698da13639799bb3fd001ec65e367
TXT-00 <txt-00@macbook-pro-6.home>
2020-07-01 10:56:29 -0700

    I added back button and the place order button works now

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutPage implements OnInit {
 
-  constructor() { }
-
+  constructor( private router : Router) { }
+  place(){
+    this.router.navigate ([ '/checkout']);
+  }
   ngOnInit() {
   }
 
